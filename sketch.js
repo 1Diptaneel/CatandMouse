b856a18e6ec7e9d0ee6e6b553a0d74e6d21bfc0c
1Diptaneel <83410205+1Diptaneel@users.noreply.github.com>
2021-07-01 09:01:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,15 +3,15 @@ var cat,catImg1,catImg2,catImg3;
 var mouse,mouseImg1,mouseImg2,mouseImg3;
 function preload() {
     //load the images here
-    bg = loadImage("images/garden.png");
+    bg = loadImage("garden.png");
 
-    catImg1 = loadAnimation("images/cat1.png");
-    catImg2 = loadAnimation("images/cat2.png","images/cat3.png");
-    catImg3 = loadAnimation("images/cat4.png");
+    catImg1 = loadAnimation("cat1.png");
+    catImg2 = loadAnimation("cat2.png","cat3.png");
+    catImg3 = loadAnimation("cat4.png");
 
-    mouseImg1 = loadAnimation("images/mouse1.png");
-    mouseImg2 = loadAnimation("images/mouse2.png","images/mouse3.png");
-    mouseImg3 = loadAnimation("images/mouse4.png");
+    mouseImg1 = loadAnimation("mouse1.png");
+    mouseImg2 = loadAnimation("mouse2.png","mouse3.png");
+    mouseImg3 = loadAnimation("mouse4.png");
 }
 
 function setup(){
@@ -22,7 +22,7 @@ function setup(){
     cat.scale = 0.2;
     
     mouse = createSprite(200,600);
-    mouse.addAnimation("mouseStanding",mouseImg1);
+    mouse.addAnimation("mouseStanding",mouseImg3);
     mouse.scale  = 0.15;
 }
 
@@ -37,7 +37,7 @@ function draw() {
         cat.x =300;
         cat.changeAnimation("catLastImage");
 
-        mouse.addAnimation("mouseLastImage", mouseImg3);
+        mouse.addAnimation("mouseLastImage", mouseImg1);
         mouse.changeAnimation("mouseLastImage");
     }  
 
